614ce90ac8d39128d395d7c31d0cb51658fdfcf6
gk <promise_ghostking@163.com>
2026-02-27 17:25:05 +0800

    feat: 添加鹰眼图路由和视图组件

--- packages/main/src/router/index.ts
+++ packages/main/src/router/index.ts
@@ -1,12 +1,22 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [
+export const routesLink = [
   {
     path: '/eagle-viewer',
-    name: 'EagleViewer',
+    name: '鹰眼图',
     component: () => import('../views/eagle-viewer.vue'),
   },
+]
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: '首页',
+    component: () => import('../views/index.vue'),
+  },
+
+  ...routesLink,
 ]
 
 const router = createRouter({
---
+++ packages/main/src/views/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { routesLink } from '../router'
+</script>
+
+<template>
+  <div>
+    <template v-for="item in routesLink" :key="item.name">
+      <router-link :to="item.path" class="mr-4 text-blue-500 hover:underline">
+        {{ item.name }}
+      </router-link>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
